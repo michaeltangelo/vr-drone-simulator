b40bd766580bee3f307895a4f6577d35298b8f35
michaeltangelo <tanman24689@gmail.com>
2016-05-11 13:22:04 -0400

    merging

---
+++ Drone Simulator/Assets/Scripts/Plane_Info.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityStandardAssets.Vehicles.Aeroplane;
+
+public class Plane_Info : MonoBehaviour {
+
+    Text instruction;
+	// Use this for initialization
+	void Start () {
+        instruction = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject AircraftPropeller = GameObject.Find("AircraftPropeller");
+        AeroplaneController script = AircraftPropeller.GetComponent <AeroplaneController> ();
+        string throttle = (script.Throttle * 100).ToString("0");
+        instruction.text = "Throttle: " + throttle;
+	}
+}
